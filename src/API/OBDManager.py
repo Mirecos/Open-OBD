3d872c5a44f904fcf413595c6bf80348b13c514c
Mich <michelrecchia1@gmail.com>
2026-01-31 21:33:21 +0100

    Reset get_speed endpoint to real data

--- src/API/OBDManager.py
+++ src/API/OBDManager.py
@@ -50,7 +50,6 @@ class OBDManager:
 
 
     def get_speed(self):
-        return 50.0
         value = self.query(obd.commands.SPEED)
         return float(value.magnitude) if value is not None else 0.0
 
